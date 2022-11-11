31ec8d5da618855965598de8a894189ee8a80842
Tony <forc3maet@gmail.com>
2022-11-11 13:25:14 +0700

    delete colorlog usage

--- .github/env.sh
+++ .github/env.sh
@@ -12,16 +12,16 @@ echo "mysql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 # Do the database setup
 if [[ $(MYSQL_PWD=${DB_PASS} mysql -u"${DB_USER}" -h"${DB_HOST}" "${DB_NAME}" -e "SHOW TABLES;") ]];then
-  colorlog "Setup MySQL:" "DB exists and connection check passed."
+  echo "Setup MySQL:" "DB exists and connection check passed."
 else
-  colorlog "Setup MySQL:" "DB does not exist or connection check failed."
-  colorlog "Setup MySQL:" "Clean old resources if exist."
+  echo "Setup MySQL:" "DB does not exist or connection check failed."
+  echo "Setup MySQL:" "Clean old resources if exist."
   MYSQL_PWD=${DB_ROOT_PASS} mysql -u"${DB_ROOT_USER}" -h"${DB_HOST}" -e \
     "
     DROP DATABASE IF EXISTS ${DB_NAME};
     DROP USER IF EXISTS ${DB_USER};
     "
-  colorlog "Setup MySQL DB:" "Creating new MySQL database and user."
+  echo "Setup MySQL DB:" "Creating new MySQL database and user."
   MYSQL_PWD=${DB_ROOT_PASS} mysql -u"${DB_ROOT_USER}" -h"${DB_HOST}" -e \
     "
     CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
@@ -29,5 +29,5 @@ else
     GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%';
     FLUSH PRIVILEGES;
     "
-  colorlog "Setup MySQL DB:" "Database created."
+  echo "Setup MySQL DB:" "Database created."
 fi
